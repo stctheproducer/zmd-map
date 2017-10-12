fcb1a41065e424094ec9eab18d94571668f60b9c
Chanda Mulenga <stconeten@gmail.com>
2017-10-12 09:11:06 +0400

    initial commit

---
+++ public_html/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Test</title>
+	<link rel="stylesheet" href="public/css/app.css">
+	<link rel="stylesheet" href="public/css/custom.css">	
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col sm-8">
+				<form action="" method="post">
+					<div class="form-group">
+						<label for="max-temp">Maximum Temperature</label>
+						<input type="text" name="max-temp" id="max-temp" class="form-control" placeholder="Max Temperature" aria-describedby="maxTempHelpId">
+						<small id="maxTempHelpId" class="text-muted">Please input the maximum temperature reading.</small>
+					</div>
+
+					<div class="form-group">
+						<label for=""></label>
+						<input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted">Help text</small>
+					</div>
+
+					<div class="form-group">
+						<label for=""></label>
+						<input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted">Help text</small>
+					</div>
+
+					<div class="form-group">
+						<label for=""></label>
+						<input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted">Help text</small>
+					</div>
+
+					<div class="form-group">
+						<label for=""></label>
+						<input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted">Help text</small>
+					</div>
+
+					<div class="form-group">
+						<label for=""></label>
+						<input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						<small id="helpId" class="text-muted">Help text</small>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+  
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+		integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>-->
+<script src="public/js/app.js"></script>
+</body>
+</html>
